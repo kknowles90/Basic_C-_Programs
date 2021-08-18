319e135f84ab36fe012796afdc1839f57819fb8b
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-08-18 15:50:50 -0400

    Add files via upload

---
+++ TryCatchFinally/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> divideby = new List<int>() { 1, 2, 3, 4, 5 };
+                Console.WriteLine("Enter a whole number to be divided by");
+                int inputint = Convert.ToInt32(Console.ReadLine());
+                foreach (int num in divideby)
+                {
+                    Console.WriteLine(inputint / num);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+            
+        }
+    }
+}
