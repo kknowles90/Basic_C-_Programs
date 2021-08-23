59de427e8b2cac46d76de7a397e010c839836aa0
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-08-23 13:45:34 -0400

    Add files via upload

--- Method3/Program.cs
+++ Method3/Program.cs
@@ -4,45 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Method3
-{
-    class Methods
-    {
 
-
-        public void method1(double var1, double var2)//takes in two double data points and does a math operation to var1 and prints var2
-        {
-
-            var1 = 12 + var1;
-            Console.WriteLine(var2);
-            
-        }
-
-
-
-
-    }
 
     class program
     {
         static void Main()
         {
             Console.WriteLine("Insert number 1:");
-            double num = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Insert number 2:");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
             
 
-            Methods n = new Methods();//calls first method
-            n.method1(num, num2);
-            
 
+            void method1(double var1, double var2)//takes in two double data points and does a math operation to var1 and prints var2
+            {
+
+                var1 = 12 + var2 + var1;
+                Console.WriteLine(var2);
+
+            }
+
+            method1(var1: x, var2: y);
+
+            //Methods n = new Methods();//calls first method
+            //n.method1(num, num2);
 
             Console.ReadLine();
-
-
-
-
 
         }
     }
