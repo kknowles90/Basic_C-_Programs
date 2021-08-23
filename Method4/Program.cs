052a69e3324ead3a5e24285a034ebd275defc04e
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-08-23 13:02:10 -0400

    Add files via upload

---
+++ Method4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method4
+{
+    class Methods
+    {
+        public int Method1(int var)//method1 runs and returns var + 12
+        {
+
+            int total1 = var + 12;
+            return total1;
+        }
+
+        public int Method1(int var1, int var2)
+        {
+            int total1 = var1 + 12 + var2;
+            return total1;
+        }
+    }
+
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Insert one or two integers:");
+            Console.WriteLine("It is optional to enter a second integer:");
+            int var1 = Convert.ToInt32(Console.ReadLine());
+            var opt = Console.ReadLine();
+     
+           
+            //Console.WriteLine("(Optional) Insert a second integer:");
+            
+            Methods n = new Methods();//calls first method and returns total
+            
+            if (opt != null)
+            {
+                int optionalvar2 = Convert.ToInt32(opt);
+                int total1 = n.Method1(var1, optionalvar2);
+                Console.WriteLine(total1);
+            }
+            else
+            {
+                int total1 = n.Method1(var1);
+                Console.WriteLine(total1);
+            }
+                    
+
+            Console.ReadLine();
+        }
+    }
+}
