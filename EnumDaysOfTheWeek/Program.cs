06bf6b64b881f0aee76124a71df7140d7618bf20
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-08-24 14:08:41 -0400

    Add files via upload

--- EnumDaysOfTheWeek/Program.cs
+++ EnumDaysOfTheWeek/Program.cs
@@ -10,13 +10,13 @@ namespace EnumDaysOfTheWeek
     {
         public enum DaysOfTheWeek
         {
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday
+            Monday,//0
+            Tuesday,//1
+            Wednesday,//2
+            Thursday,//3
+            Friday,//4
+            Saturday,//5
+            Sunday//6
         }
         static void Main(string[] args)
         {
@@ -24,22 +24,29 @@ namespace EnumDaysOfTheWeek
             try
             {
                 Console.WriteLine("Please enter an actual day of the week.");
-                string day = Console.ReadLine();
+                
+                var day = Console.ReadLine();
+                string[] values = Enum.GetNames(typeof(DaysOfTheWeek));//creates a 1D array to store enum values
                 for ( int i = 0; i < 6; i++)
                 {
-                    if (day == DaysOfTheWeek.[i])
+                    var wd = (DaysOfTheWeek)i;
+                    if (day == values[i])
                     {
-                        Console.WriteLine(DaysOfTheWeek.[i]);
+                        Console.WriteLine((DaysOfTheWeek)i);//outputs the string value of the enum
+                        int intvalue = (int)(DaysOfTheWeek)i;//converts enum to int value for the specific day
+                        Console.WriteLine("That day has a Enum value of " + intvalue);//outputs the int value of the enum
+                         
                     }
                 }
         
             }
-            catch (ArgumentException e)
+            catch (Exception ex)//catches any exceptions
             {
                 Console.WriteLine("Please insert a valid day.");
             }
 
             Console.ReadLine();
+            
         
         
         
