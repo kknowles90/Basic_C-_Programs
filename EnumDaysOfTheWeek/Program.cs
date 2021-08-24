63060527b3cfd8dc017900e7b2353e2feb35a92b
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-08-24 13:33:06 -0400

    Add files via upload

---
+++ EnumDaysOfTheWeek/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDaysOfTheWeek
+{
+    class Program
+    {
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+ 
+            try
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                string day = Console.ReadLine();
+                for ( int i = 0; i < 6; i++)
+                {
+                    if (day == DaysOfTheWeek.[i])
+                    {
+                        Console.WriteLine(DaysOfTheWeek.[i]);
+                    }
+                }
+        
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Please insert a valid day.");
+            }
+
+            Console.ReadLine();
+        
+        
+        
+        }
+         
+    }
+}
