de4a4be1a33a5c9a579ab8390a812b6e5a8da728
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-08-24 21:20:31 -0400

    Add files via upload

--- EnumDaysOfTheWeek/Program.cs
+++ EnumDaysOfTheWeek/Program.cs
@@ -20,30 +20,37 @@ namespace EnumDaysOfTheWeek
         }
         static void Main(string[] args)
         {
- 
+
             try
             {
                 Console.WriteLine("Please enter an actual day of the week.");
-                
-                var day = Console.ReadLine();
-                string[] values = Enum.GetNames(typeof(DaysOfTheWeek));//creates a 1D array to store enum values
-                for ( int i = 0; i < 7; i++)
-                {
-                    
-                    if (day == values[i])
-                    {
-                        Console.WriteLine((DaysOfTheWeek)i);//outputs the string value of the enum
-                        int intvalue = (int)(DaysOfTheWeek)i;//converts enum to int value for the specific day
-                        Console.WriteLine("That day has a Enum value of " + intvalue);//outputs the int value of the enum
-                         
-                    }
-                    else if (i == 6)
-                    {
-                        throw new ArgumentException();//I think this will throw an exception that the catch will catch?
-                    }
-                }
-        
+
+                var dayinput = Console.ReadLine();
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), dayinput);
+
+                Console.WriteLine("It is {0}", day);
             }
+                //Console.WriteLine("Please enter an actual day of the week.");
+                
+                //var day = Console.ReadLine();
+                //string[] values = Enum.GetNames(typeof(DaysOfTheWeek));//creates a 1D array to store enum values
+                //for ( int i = 0; i < 7; i++)
+                //{
+                    
+                    //if (day == values[i])
+                    //{
+                      //  Console.WriteLine((DaysOfTheWeek)i);//outputs the string value of the enum
+                        //int intvalue = (int)(DaysOfTheWeek)i;//converts enum to int value for the specific day
+                        //Console.WriteLine("That day has a Enum value of " + intvalue);//outputs the int value of the enum
+                         
+                    //}
+                    //else if (i == 6)
+                    //{
+                    //    throw new ArgumentException();//I think this will throw an exception that the catch will catch?
+                    //}
+                //}
+        
+            
             catch (ArgumentException ex)//catches any exceptions
             {
                 Console.WriteLine("Please insert a valid day.");
