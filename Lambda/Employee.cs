2dbe45d67b49d1de59fe22e6837fd2166b6d5fd5
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-08-25 19:55:47 -0400

    Add files via upload

---
+++ Lambda/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Employee
+    {
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public double ID { get; set; }
+
+        public Employee(string firstName, string lastName, double ID)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.ID = ID;
+        }
+    }
+}
--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -6,45 +6,74 @@ using System.Threading.Tasks;
 
 namespace Lambda
 {
-    public class Employee
+    public class Program
     {
+        
         public static void Main(string[] args)
         {
-        public String lastName { get; set; }
-        public String firstName { get; set; }
-        public double ID { get; set; }
+            List<Employee> empList = new List<Employee>();
 
-        public Employee(String firstName, String lastName, double ID)
-        {
-            this.lastName = lastName;
-            this.firstName = firstName;
-            this.ID = ID;
+            Employee num1 = new Employee("Joe", "Smow", 1);
+            Employee num2 = new Employee("Joe", "Biden", 2);
+            Employee num3 = new Employee("Kyle", "Lawerance", 3);
+            Employee num4 = new Employee("Samantha", "Emily", 4);
+            Employee num5 = new Employee("John", "Snow", 5);
+            Employee num6 = new Employee("Donald", "Trump", 6);
+            Employee num7 = new Employee("Adam", "Sandler", 7);
+            Employee num8 = new Employee("Alex", "Ronalds", 8);
+            Employee num9 = new Employee("Johnny", "Cash", 9);
+            Employee num10 = new Employee("Malcom", "Smith", 10);
+
+            empList.Add(num1);
+            empList.Add(num2);
+            empList.Add(num3);
+            empList.Add(num4);
+            empList.Add(num5);
+            empList.Add(num6);
+            empList.Add(num7);
+            empList.Add(num8);
+            empList.Add(num9);
+            empList.Add(num10);
+
+            
+
+            foreach (Employee emp in empList)
+            {
+                var fname = emp.firstName;
+                var lname = emp.lastName;
+                var ID = emp.ID;
+                
+                if (emp.firstName == "Joe")
+                {
+                    List<Employee> nameList = new List<Employee>();
+                    Employee matchName = new Employee(fname, lname, ID);
+                    nameList.Add(matchName);
+                    //Console.WriteLine(fname, lname, ID);
+                }
+            }
+            
+            List<Employee> newlist = empList.Where(x => x.firstName == "Joe").ToList();
+
+            List<Employee> numlist = empList.Where(x => x.ID > 5).ToList();
+            
+            Console.ReadLine();
         }
+    }
+}
+    
 
-        List<Employee> empList = new List<Employee>();
 
-        Employee num1 = new Employee("Joe", "Smow", 1);
-        Employee num2 = new Employee("Joe", "Biden", 2);
-        Employee num3 = new Employee("Kyle", "Lawerance", 3);
-        Employee num4 = new Employee("Samantha", "Emily", 4);
-        Employee num5 = new Employee("John", "Snow", 5);
-        Employee num6 = new Employee("Donald", "Trump", 6);
-        Employee num7 = new Employee("Adam", "Sandler", 7);
-        Employee num8 = new Employee("Alex", "Ronalds", 8);
-        Employee num9 = new Employee("Johnny", "Cash", 9);
-        Employee num10 = new Employee("Malcom", "Smith", 10);
+            
 
-        empList.Add(num1);
 
-        foreach (String Name in firstName)
-        {
-
-        }
-
+        
         //In the Main() method, create a list of at least 10 employees.Each employee should have a first and last name, as well as an Id.At least two employees should have the first name “Joe”.
 
 
         //Using a foreach loop, create a new list of all employees with the first name “Joe”.
-    }
-}
+
+//Perform the same action again, but this time with a lambda expression.
+
+//Using a lambda expression, make a list of all employees with an Id number greater than 5.
+
 
