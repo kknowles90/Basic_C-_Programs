28fc9a5dbc6a73504e1358f887234e8e85d87b5d
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-08-25 21:49:26 -0400

    Add files via upload

---
+++ DateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);//The current time displayed as output
+            Console.WriteLine("Insert an amount of hours to be added to the current date:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            DateTime current = DateTime.Now;
+            DateTime hours = current.AddHours(x);//The inputted hours from user
+            Console.WriteLine("The requested time would be: " + hours);// the output would be in the form of "The requested time would be: (time inserted here)"
+            Console.ReadLine();
+            
+        }
+    }
+}
