eee63f482545e51f9ce524f49e7ef04b555e8031
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-08-27 10:59:57 -0400

    Add files via upload

---
+++ ConstVar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string example = "This is a constant string";//Creates a constant variable under example
+
+            var example2 = "this is a variable";//creates a var called example2
+
+        }
+    }
+}
---
+++ ConstVar/constructors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Student
+    {
+        string _Id = "";
+        string _FName = "";
+        string _LName = "";
+
+        public Student(string id)
+            : this(id, "", "")//chained constructor with 1
+        {
+
+        }
+
+        public Student(string id, string fName)
+            : this(id, fName, "")//Chained constructor with 2
+        {
+
+        }
+
+        public Student(string id, string fName, string lName)
+        {
+            _Id = id;
+            _FName = fName;
+            _LName = lName;
+        }
+
+    }
+}
