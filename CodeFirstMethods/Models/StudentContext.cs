9b94f39f662eaac3db20bf6d8ef0f5bcb5303fd9
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2021-09-03 14:14:51 -0700

    CodeFrist

---
+++ CodeFirstMethods/Models/StudentContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CodeFirstMethods.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("StudentDatabase")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
